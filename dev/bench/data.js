f9c85abac8b85ac5708abed9eebabe4ee11bbfba
github-action-benchmark <github@users.noreply.github.com>
2022-07-19 09:10:23 +0000

    add Benchmark (cargo) benchmark result for 27e0a64e908576c5356dfca0d89869a3bd472d08

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1658221823096,
+  "repoUrl": "https://github.com/rmw-lib/rust_tmpl",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "i@rmw.link",
+            "name": "gcxfd",
+            "username": "gcxfd"
+          },
+          "committer": {
+            "email": "i@rmw.link",
+            "name": "gcxfd",
+            "username": "gcxfd"
+          },
+          "distinct": true,
+          "id": "27e0a64e908576c5356dfca0d89869a3bd472d08",
+          "message": "🔶",
+          "timestamp": "2022-07-19T17:07:41+08:00",
+          "tree_id": "561f5ea3b21eb20c64a8185a56493358cccec991",
+          "url": "https://github.com/rmw-lib/rust_tmpl/commit/27e0a64e908576c5356dfca0d89869a3bd472d08"
+        },
+        "date": 1658221822418,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "add",
+            "value": 9,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
