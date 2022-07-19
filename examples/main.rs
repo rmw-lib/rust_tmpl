8c569d1140ddf03da3692b59cb6ec8e2382e92ec
gcxfd <i@rmw.link>
2022-07-19 17:04:35 +0800

    init

--- examples/main.rs
+++
@@ -1,7 +0,0 @@
-use anyhow::Result;
-use rust_tmpl::add;
-
-fn main() -> Result<()> {
-    dbg!(add(1, 2));
-    Ok(())
-}
--- sh/upgrade.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-cargo update
-cargo upgrade --workspace
