06e107d5b91c772ccd0d3653e02753a8a0af8bf5
gcxfd <i@rmw.link>
2022-07-21 18:14:19 +0800

    🔶

--- rpl.sh
+++ rpl.sh
@@ -3,9 +3,34 @@
 DIR=$(dirname $(realpath "$0"))
 cd $DIR
 
-sd $1 $2 ./.git/config
+if  [ ! -n "$1" ] ;then
+exit 1
+fi
+
+if  [ ! -n "$2" ] ;then
+exit 1
+fi
+
+# https://github.com/chmln/sd
+
+add(){
+for i in $*
+do
+  if ! hash $1 2>/dev/null; then
+    cargo install $2
+  fi
+done
+}
+
+add fd sd
+
+rpl(){
+  echo $3
+  sd $1 $2 $3
+}
 
 for file in $(fd --type file); do
-  echo $file
-  sd $1 $2 "$file"
+  rpl $1 $2 "$file"
 done
+
+rpl $1 $2 ./.git/config
